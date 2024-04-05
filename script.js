aba06677e5424bec7ab71ea38d6244d681a6911e
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-04-05 16:20:06 +0800

    Update if else statements by showing what the player and the computer chose

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const getComputerChoice = () => game[Math.floor(Math.random() * game.length)].to
 
 const playerPrompt = prompt("Rock, Paper, or Scissors?: ").toUpperCase();
 
-const playRound = (playerSelection, computerSelection) => {
+
+
+let playRound = (playerSelection, computerSelection) => {
     if(playerSelection == "ROCK" && computerSelection == "SCISSORS" || playerSelection == "PAPER" && computerSelection == "ROCK" || playerSelection == "SCISSORS" && computerSelection == "PAPER"){
-        return "You Win! " + playerSelection + " beats " + computerSelection;
+        return "You Win! You chose " + playerSelection + " and Computer chose " + computerSelection;
     }
     else if(playerSelection == "ROCK" && computerSelection == "PAPER" || playerSelection == "PAPER" && computerSelection == "SCISSORS" || playerSelection == "SCISSORS" && computerSelection == "ROCK"){
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+        return "You Lose! Computer chose " + computerSelection + " and you chose " + playerSelection;
     }
     else if (playerSelection == computerSelection){
         return "Draw! You both chose " + playerSelection;
