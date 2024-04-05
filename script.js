70ecc849c22199b5ef778bad128809284bd89003
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-04-05 14:22:15 +0800

    Add a function that returns a string declaring the winner or a tie

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ let game = ["Rock","Paper","Scissors"];
 
 let =  getComputerChoice = () => game[Math.floor(Math.random() * game.length)]
 
+const player = "paper";
 
-console.log(getComputerChoice())
+let playRound = (playerSelection, computerSelection) => "You Lose! " + playerSelection + " beats " + computerSelection;
+
+console.log(playRound(player.toUpperCase(), getComputerChoice().toUpperCase()));
