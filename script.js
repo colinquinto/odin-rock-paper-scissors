0cc00bbb3d38f6ef7518514eb4635db44de9a681
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-04-05 15:54:39 +0800

    Add if else statement and conditions that specifies whether the player wins, loses, draws or inputs invalid values

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let game = ["Rock","Paper","Scissors"];
+const game = ["Rock","Paper","Scissors"];
 
-let =  getComputerChoice = () => game[Math.floor(Math.random() * game.length)]
+const getComputerChoice = () => game[Math.floor(Math.random() * game.length)].toUpperCase();
 
-const player = "paper";
+const playerPrompt = prompt("Rock, Paper, or Scissors?: ").toUpperCase();
 
-let playRound = (playerSelection, computerSelection) => "You Lose! " + playerSelection + " beats " + computerSelection;
+const playRound = (playerSelection, computerSelection) => {
+    if(playerSelection == "ROCK" && computerSelection == "SCISSORS" || playerSelection == "PAPER" && computerSelection == "ROCK" || playerSelection == "SCISSORS" && computerSelection == "PAPER"){
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+    else if(playerSelection == "ROCK" && computerSelection == "PAPER" || playerSelection == "PAPER" && computerSelection == "SCISSORS" || playerSelection == "SCISSORS" && computerSelection == "ROCK"){
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+    else if (playerSelection == computerSelection){
+        return "Draw! You both chose " + playerSelection;
+    }
+    else {
+        return "Invalid selection. Please input Rock, Paper, or Scissors only.";
+    }
+}
 
-console.log(playRound(player.toUpperCase(), getComputerChoice().toUpperCase()));
+console.log(playRound(playerPrompt, getComputerChoice()));
